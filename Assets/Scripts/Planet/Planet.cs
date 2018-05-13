068b16b3dc5aa782c1a46b6b1bac7f8ff78cac8a
Basmeuw <32522618+Basmeuw@users.noreply.github.com>
2018-05-13 21:17:16 +0200

    Planet can read its info from a txt file

---
+++ Assets/Scripts/Planet/Planet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	public TextAsset planetInfo;
+	public string name;
+	public string description;
+
+	void Awake(){
+
+
+		string planetText = planetInfo.text;
+		int seperator = planetText.IndexOf('\n');
+
+		if(seperator > -1){
+			name = planetText.Substring(0, seperator);
+			description = planetText.Substring(seperator+1);
+		}
+		
+		Debug.Log(name);
+		Debug.Log(description);
+	}
+
+	public void visit(){
+
+	}
+
+}
