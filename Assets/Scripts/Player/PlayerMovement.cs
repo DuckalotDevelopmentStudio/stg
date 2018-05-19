0fa3ae8f032234a60c42760e052c4d0f02b9952f
Stef Halmans <stefhalmans@ziggo.nl>
2018-05-19 14:28:05 +0200

    Player Movement
    Added player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float MovementSpeed;                                                         
+    public float RotationSpeed;
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);      // Moves the player forward
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(- Vector3.forward * MovementSpeed * Time.deltaTime);    // Moves the player back
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);               // Rotates the player to the left
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, -RotationSpeed * Time.deltaTime);              // Rotates the player to the right
+        }
+    }
+}
