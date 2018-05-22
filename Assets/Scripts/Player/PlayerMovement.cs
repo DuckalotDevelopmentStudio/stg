6ff3ada58ea22cbc3187c8b0d9858db8ba901650
Stef Halmans <stefhalmans@ziggo.nl>
2018-05-22 19:36:29 +0200

    Fuel
    Added fuel and fixed the rotation for the movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,25 +2,33 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float MovementSpeed;                                                         
+    public float ForwardMovementSpeed;
+    public float BackMovementSpeed;
     public float RotationSpeed;
-
+    public float MovementFuelCost;
+    public float RotationFuelCost;
+    public float Fuel = 100;
+     
     void FixedUpdate() {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);      // Moves the player forward
+            transform.Translate(Vector3.forward * ForwardMovementSpeed * Time.deltaTime);      // Moves the player forward
+            Fuel -= MovementFuelCost;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(- Vector3.forward * MovementSpeed * Time.deltaTime);    // Moves the player back
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);               // Rotates the player to the left
+            transform.Translate(- Vector3.forward * BackMovementSpeed * Time.deltaTime);    // Moves the player back
+            Fuel -= MovementFuelCost;
         }
         if(Input.GetKey(KeyCode.D))
         {
+            transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);               // Rotates the player to the left
+            Fuel -= RotationFuelCost;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.up, -RotationSpeed * Time.deltaTime);              // Rotates the player to the right
+            Fuel -= RotationFuelCost;
         }
     }
 }
